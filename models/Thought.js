466e9f61347a7602681ac5e0b3342eb8635ced1d
andrew clark <andy-clark316@hotmail.com>
2023-06-18 16:30:12 +1000

    added database model code

---
+++ models/Thought.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create User model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: { type: String, required: true, },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+    username: { type: String, required: true, },
+    reactions: [
+        {
+          reaction:{ 
+            type: String,
+        }
+        },
+      ],
+    },
+    {
+      toJSON: {
+        getters: true,
+      },
+      id: false,
+    }
+  );
+
+// Create a virtual property `fullName` that gets and sets the user's full name
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+  });
+
+// Initialize our User model
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
